78283549a572c908b71303ceeb694909d714b057
Carlo Carabeo <carlo.carabeo@gmail.com>
2015-03-15 18:52:36 +0800

    active sessions untested

--- application/controllers/logout.php
+++ application/controllers/logout.php
@@ -7,11 +7,18 @@ class Logout extends CI_Controller{
 	//destroys all session data then redirects to login page
 	function __construct(){
 		parent::__construct();
+		$id = $this->session->userdata('id');
+
+		//destroy session
 		$this->session->unset_userdata('id');
 		$this->session->unset_userdata('is_logged_in');	
 		$this->session->unset_userdata('is_admin');	
 		$this->session->unset_userdata('college');	
 		$this->session->sess_destroy();
+
+		//remove session from db
+		$this->load->model('active_sessions_model');
+		$this->active_sessions_model->_removeSession($id);
 		redirect('login');
 	}
 	
--- application/models/active_sessions_model.php
+++ application/models/active_sessions_model.php
@@ -7,10 +7,20 @@ class Active_Sessions_Model extends CI_Model{
 	}
 
 	function _addSession($id){
+		$arr = array(
+			'session_id' => md5($id),
+			'voter_id' => $id,
+			'session_ip_address' => $_SERVER['REMOTE_ADDR'],
+			'timestamp' => time()
+		);
+		$this->db->insert('active_sessions', $arr);
 		return;
 	}
 
-	function _removeSession(){
-		return;
+	function _removeSession($id){
+		$this->db->where('session_id', md5($id));
+		$this->db->where('voter_id', $id);
+
+		$this->db->delete('active_sessions');
 	}
 }
