1fbe18c67c4f814d4e7ecea6696a8acc3e0a7904
Carlo Carabeo <carlo.carabeo@gmail.com>
2015-03-17 16:19:57 +0800

    remove hardcoded functions

--- application/controllers/voter.php
+++ application/controllers/voter.php
@@ -33,14 +33,14 @@ class Voter extends CI_Controller {
 		$data['studentName'] = $this->_getStudentName($this->session->userdata('id'));
 		$data['college'] = $this->session->userdata('college');
 		//candidates
-		$data['usg_pres'] = $this->_get_USG_Pres();
-		$data['usg_vp_internal'] = $this->_get_USG_VPInternal();
-		$data['usg_vp_external'] = $this->_get_USG_VPExternal();
-		$data['usg_treasurer'] = $this->_get_USG_Treasurer();
-		$data['usg_secretary'] = $this->_get_USG_Secretary();
-		$data['stc_campus_pres'] = $this->_get_STC_CampusPres();
+		$data['usg_pres'] = $this->candidate_model->_getCandidatesFor('USG President');
+		$data['usg_vp_internal'] = $this->candidate_model->_getCandidatesFor('VP Internals');
+		$data['usg_vp_external'] = $this->candidate_model->_getCandidatesFor('VP Externals');
+		$data['usg_treasurer'] = $this->candidate_model->_getCandidatesFor('Executive Treasurer');
+		$data['usg_secretary'] = $this->candidate_model->_getCandidatesFor('Executive Secretary');
+		$data['stc_campus_pres'] = $this->candidate_model->_getCandidatesFor('STC President');
 		$data['stc_college_rep'] = $this->_get_STC_CollegeRep($this->session->userdata('college'));
-		$data['stc_la_rep'] = $this->_get_STC_LARep();
+		$data['stc_la_rep'] = $this->candidate_model->_getCandidatesFor('Legislative Assembly Representative');
 
 		$this->load->view('includes/template', $data);
 	}
@@ -67,7 +67,6 @@ class Voter extends CI_Controller {
 		$this->session->unset_userdata('college');	
 		$this->session->sess_destroy();
 		
-
 		$this->success();
 	}
 
@@ -79,37 +78,7 @@ class Voter extends CI_Controller {
 		header('refresh: 5; url=' . _controller_url() . 'logout');
 	}
 
-	//get candidates functions 
-	//This part feels hardcoded i should feel bad about myself -beng
-	function _get_USG_Pres(){
-		return $this->candidate_model->_getCandidatesFor('USG President');
-	}
-
-	function _get_USG_VPInternal(){
-		return $this->candidate_model->_getCandidatesFor('VP Internals');
-	}
-
-	function _get_USG_VPExternal(){
-		return $this->candidate_model->_getCandidatesFor('VP Externals');
-	}
-
-	function _get_USG_Treasurer(){
-		return $this->candidate_model->_getCandidatesFor('Executive Treasurer');
-	}
-
-	function _get_USG_Secretary(){
-		return $this->candidate_model->_getCandidatesFor('Executive Secretary');
-	}
-
-	function _get_STC_CampusPres(){
-		return $this->candidate_model->_getCandidatesFor('STC President');
-	}
-
 	function _get_STC_CollegeRep($college){
 		return $this->candidate_model->_getCollegeRep($college);
-	}
-
-	function _get_STC_LARep(){
-		return $this->candidate_model->_getCandidatesFor('Legislative Assembly Representative');
 	}
 }
